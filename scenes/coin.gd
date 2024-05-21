8448e61b5ddeaa9ff41175fe751b7795b1a717c4
jcontreerass <jorgecm.2004@gmail.com>
2024-05-21 22:28:13 +0200

    solucionando_bugs

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 func _on_body_entered(_body):
 	$sonido_moneda.play()
-	$AnimatedSprite2D.play()
 	if _body.name == "Player":
 		queue_free()
+
+func _process(delta):
+	$AnimatedSprite2D.play("new_animation")
--- scenes/video_menu.gd
+++ scenes/video_menu.gd
@@ -4,13 +4,15 @@ extends Control
 
 
 const SLIDES = {
-	"primera": {
+	"PRIMERA": {
 		"label": "Usa las flechas/WASD/mando para moverte",
 		"image": "res://images/Captura de pantalla 2024-05-20 221001.1.png",
+		"button_label": "SEGUNDA",
 		},
-	"segunda":{
+	"SEGUNDA":{
 		"label": "Recoge todas las monedas para conseguir pasar el nivel",
 		"image": "res://images/Captura de pantalla 2024-05-20 221113.png",
+		"button_label": "BACK",
 		}
 	}
 
@@ -18,13 +20,16 @@ var current_slide
 
 func _ready():
 	MenuMusic.play_music_level()
-	set_slide("primera")
+	set_slide("PRIMERA")
 
 func set_slide(slide: String) -> void:
 	current_slide = SLIDES[slide]
 	$Label.text = current_slide.label
 	$TextureRect.texture = load(current_slide.image)
-
+	$Button_SIGUIENTE.text = current_slide.button_label
 
 func _on_button_siguiente_pressed():
-	set_slide("segunda")
+	set_slide(SLIDES[$Button_SIGUIENTE.text])
+	if SLIDES["SEGUNDA"]:
+		if _on_button_siguiente_pressed():
+			get_tree().change_scene_to_file("res://options_menu.tscn")
